b7708b7b07140c05b74d24aac5219e77be4c1baa
Numakunai_Shunichi <Shun99996@gmail.com>
2022-02-06 06:27:41 +0900

    test commit

--- web/init.sh
+++ web/init.sh
@@ -1,3 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-/usr/sbin/httpd -DBACKGROUND
+# -----------------------------------------------------------------------------------------------------------------------------
+# systemctl実行
+# -----------------------------------------------------------------------------------------------------------------------------
+exec /sbin/init
+
+# -----------------------------------------------------------------------------------------------------------------------------
+# MySQLの初期設定
+# -----------------------------------------------------------------------------------------------------------------------------
+# 初期パスワードの確認
+# DB_INITPASS=$(grep "A temporary password is generated" /var/log/mysqld.log | sed -e "s/.*root@localhost: //")
+# 入手した初期パスワードでログインし、パスワードポリシーを下げる
+# mysql --connect-expired-password -uroot -p"${DB_INITPASS}" -e "set global validate_password_policy=LOW;"
+# 初期パスワードを変更する
+# mysql --connect-expired-password -uroot -p"${DB_INITPASS}" -e "alter user 'root'@'localhost' identified by 'snstoola00';"
+# sqlファイルを流すDBを作成する
+# mysql --connect-expired-password -uroot -p'snstoola00' -e 'CREATE DATABASE snstoola00;'
+
+# Apache起動
+#systemctl start httpd.service 
